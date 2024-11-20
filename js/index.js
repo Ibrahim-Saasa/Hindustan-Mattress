24bf28a51888ef6e09d6978016630d8f1a07cbb3
Ibrahim-Saasa <ibrahimsaasa@gmail.com>
2024-11-20 16:11:16 +0530

    added contact and cart page

--- js/index.js
+++ js/index.js
@@ -13,3 +13,27 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+function sendToWhattsapp() {
+  let number = "+919421035852";
+
+  let name = document.getElementById("text").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  var url =
+    "https://wa.me/" +
+    number +
+    "?text=" +
+    "Name : " +
+    name +
+    "%0a" +
+    "Email : " +
+    email +
+    "%0a" +
+    "Message : " +
+    message +
+    "%0a%0a";
+
+  window.open(url, "_blank").focus();
+}
